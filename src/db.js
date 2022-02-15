e6e618060548f8de6674800da28cd117dd9c1dcd
borat55 <boram8655@gmail.com>
2022-02-14 21:01:04 -0300

    creating schema

--- src/db.js
+++ src/db.js
@@ -1,4 +1,13 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://127.0.0.1:27017/wetube")
+mongoose.connect("mongodb://127.0.0.1:27017/wetube", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true});
 
+const handleOpen = () => console.log(
+    "connected to the db"
+)
+const handleError =(error) =>console.log("DB Error", error)
+const db = mongoose.connection;
+db.on("error", handleError)
+db.once("open", handleOpen)
