c55724ba6ad4902dd02b25f0942787999fb71c36
borat55 <boram8655@gmail.com>
2022-02-15 16:54:07 -0300

    revising lines in videoCOntroller for the error page, revising getEdit fundtion and the form in edit.pug file

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -9,14 +9,20 @@ export const home = async (req, res) => {
 export const watch = async(req, res) => {
     const { id } = req.params;
     const video = await Video.findById(id);
-    if (video) {
-    return res.render("watch", { pageTitle: `Watching: ${video.title}`, video })
-    } else {return res.render("404", {pageTitle: `Error: Video not found`})};
+    if (!video) {
+        return res.render("404", {pageTitle: `Error: Video not found`})
+    }
+    return res.render("watch", { pageTitle: `Watch: ${video.title}`, video })
+    
 };
 
-export const getEdit = (req, res) => {
+export const getEdit = async(req, res) => {
     const { id } = req.params;
-    return res.render("edit", { pageTitle: `Editing` })
+    const video = await Video.findById(id);
+    if (!video) {
+        return res.render("404", {pageTitle: `Error: Video not found`})
+    }
+    return res.render("edit", { pageTitle: `Edit: ${video.title}`, video })
 };
 
 export const postEdit = (req, res) => {
