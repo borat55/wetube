597b53ae44eda9b92e11c5d6e5a454b213865d1c
borat55 <boram8655@gmail.com>
2022-06-08 15:38:09 -0300

    fixing imgs and videos to be loaded

--- src/server.js
+++ src/server.js
@@ -21,14 +21,14 @@ app.use((req, res, next) => {
     next();
   });
 
-// app.use((req, res, next) => {
-//     res.setHeader("Access-Control-Allow-Origin", "*");
-//     res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//     );
-//     next();
-//     });
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+    });
     
 
 app.use((req, res, next) => {
