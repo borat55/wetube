81f4a4378d8a272bf6588e2f9963d5267be9f3e0
borat55 <boram8655@gmail.com>
2022-06-09 11:37:52 -0300

    Fixing the video&img tags to show the images or videos in development envirenment

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -51,6 +51,7 @@ const formatTime = (seconds) =>
     new Date(seconds * 1000).toISOString().substr(14, 5);
 
 const handleLoadedMetadata = () => {
+    console.log(video.duration)
     totalTime.innerText = formatTime(Math.floor(video.duration));
     timeline.max = Math.floor(video.duration);
 }
